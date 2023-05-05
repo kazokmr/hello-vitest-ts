0f0dee03ec478aa6a559d3b59ace83df31211d7f
kazokmr <engkokmr@gmail.com>
2023-05-06 06:52:49 +0900

    [2.4.5 テストを並列で実行]まで

---
+++ src/chapter2/group/concurrent.test.ts
@@ -0,0 +1,14 @@
+import {expect, test} from "vitest";
+
+const fetchData = () => new Promise(resolve => setTimeout(resolve, 1000, "lemon"));
+
+test.concurrent.each(
+    Array.from(new Array(100).keys()).map(n => ({
+        n,
+        expected: "lemon"
+    })),
+)(`fetchData $n`, async ({n, expected}) => {
+        console.log(n);
+        await expect(fetchData()).resolves.toBe(expected);
+    }
+);
