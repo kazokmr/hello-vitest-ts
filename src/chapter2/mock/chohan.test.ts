94f46087ef4b32922b0e89b2c4f7a8f0e19b288b
kazokmr <engkokmr@gmail.com>
2023-05-06 11:31:37 +0900

    2.6.2 vi.mock() を利用したモック化 2.6.3 vi.spyOn() を利用したモック化

---
+++ src/chapter2/mock/chohan.test.ts
@@ -0,0 +1,20 @@
+import {describe, expect, test, vi} from "vitest";
+import {chohan} from "./chohan.ts";
+
+// seed関数をMock化する
+vi.mock("./seed", () => {
+    return {
+        seed: vi.fn()
+            .mockImplementationOnce(() => 2)    // 1回目は 2（偶数）を出す
+            .mockImplementationOnce(() => 1),   // 2回目は 1 (奇数) を出す
+    };
+});
+
+describe("chohan", () => {
+    test("returns 丁 when seed returns an even number like 2", () => {
+        expect(chohan()).toBe("丁");
+    });
+    test("returns 半 when seed returns an odd number like 1", () => {
+        expect(chohan()).toBe("半");
+    });
+});
---
+++ src/chapter2/mock/chohan.ts
@@ -0,0 +1,3 @@
+import {seed} from "./seed.ts";
+
+export const chohan = () => seed() % 2 === 0 ? "丁" : "半"
