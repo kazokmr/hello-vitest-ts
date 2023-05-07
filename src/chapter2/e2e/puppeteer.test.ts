5fe7d74b74ecb71631b560038c036158cd4f4736
kazokmr <engkokmr@gmail.com>
2023-05-07 19:21:21 +0900

    2.8.6 Puppeteer を利用した E2E テスト

---
+++ src/chapter2/e2e/puppeteer.test.ts
@@ -0,0 +1,29 @@
+import {afterAll, beforeAll, describe, expect, test} from "vitest";
+import puppeteer, {Browser} from "puppeteer";
+
+describe("e2e test with puppeteer", () => {
+    let browser: Browser;
+
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    test("a search keyword will be on the page title in google.com", async () => {
+
+        const page = await browser.newPage();
+        await page.goto("https://www.google.com/ncr");
+
+        await page.type("textarea[name='q']", "puppeteer");
+        await page.keyboard.press("Enter");
+
+        await page.waitForNavigation({
+            timeout: 2000,
+            waitUntil: "domcontentloaded",
+        });
+        expect(await page.title()).toBe("puppeteer - Google Search");
+    });
+});
