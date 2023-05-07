055274addfb50fecd64f2c2bc842345233d8b51d
kazokmr <engkokmr@gmail.com>
2023-05-07 17:04:45 +0900

    2.7.4 Storybookの活用

---
+++ src/chapter2/ui/Button.stories.tsx
@@ -0,0 +1,24 @@
+import {Button} from "./Button";
+import {Meta, StoryObj} from "@storybook/react";
+import {userEvent, within} from "@storybook/testing-library";
+
+const meta = {
+    component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary = {} satisfies Story;
+
+export const Secondary = {
+    args: {primary: false}
+} satisfies Story;
+
+export const ClickButton = {
+    play: async ({canvasElement}) => {
+        const canvas = within(canvasElement);
+        const button = canvas.getByRole("button", {name: /(ON|OFF)/});
+        await userEvent.click(button);
+    }
+} satisfies Story;
