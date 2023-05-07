1e8557b3d8da55a0f2a2fdf0fe508b5eb98418d8
kazokmr <engkokmr@gmail.com>
2023-05-07 15:59:42 +0900

    2.7.3 React Testing Library を利用したUIテスト

--- src/chapter2/ui/Button.test.tsx
+++ src/chapter2/ui/Button.test.tsx
@@ -1,10 +1,21 @@
 import {describe, expect, test} from "vitest";
 import * as renderer from "react-test-renderer";
 import {Button} from "./Button";
+import {render} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("Button", () => {
     test("renders correctly with react-test-renderer", () => {
         const button = renderer.create(<Button/>)
         expect(button.toJSON()).toMatchSnapshot();
+    });
+
+    test("changes the button text upon clicking the button using React Testing Library", async () => {
+        const button = render(<Button/>);
+        const user = userEvent.setup();
+        await user.click(button.getByRole("button", {name: "ON"}));
+        expect(button.getByRole("button", {name: "OFF"})).toBeTruthy();
+        await user.click(button.getByRole("button", {name: "OFF"}));
+        expect(button.getByRole("button", {name: "ON"})).toBeTruthy();
     });
 });
