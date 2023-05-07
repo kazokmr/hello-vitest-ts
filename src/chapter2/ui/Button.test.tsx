30aa69bf2240bda739f9ad3c27878c2206c6c17f
kazokmr <engkokmr@gmail.com>
2023-05-07 15:31:46 +0900

    2.7.2 スナップショットテスト

---
+++ src/chapter2/ui/Button.test.tsx
@@ -0,0 +1,10 @@
+import {describe, expect, test} from "vitest";
+import * as renderer from "react-test-renderer";
+import {Button} from "./Button";
+
+describe("Button", () => {
+    test("renders correctly with react-test-renderer", () => {
+        const button = renderer.create(<Button/>)
+        expect(button.toJSON()).toMatchSnapshot();
+    });
+});
