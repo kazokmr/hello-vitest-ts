8466a1da309df5e2298f847bca8c653d9d94a231
kazokmr <engkokmr@gmail.com>
2023-05-08 18:02:29 +0900

    4.3.6 コードカバレッジの活用

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import {defineConfig} from "vitest/config";
 export default defineConfig({
     test: {
         environment: "jsdom",
-        css: true
+        css: true,
+        coverage: {
+            provider: "c8",
+            reporter: ["html"]
+        }
     }
 })
